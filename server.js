af4e5db5c454958a29266a74fdf09cbc34ef8dc7
Matt Ilagan <matthew@bluzelle.com>
2019-01-06 17:46:48 -0800

    Add back name storage functionality

--- server.js
+++ server.js
@@ -147,31 +147,26 @@ app.post('/heroku/resources', function handleProvisioning(req, res) {
           .then(res => res.json())
           .then(function(response){
             console.log(response);
+
+            let blzObj = bluzelle({
+              entry: "ws://bernoulli.bluzelle.com:51010",
+              uuid: "herokubluzelleaddonapps",
+              private_pem: "MHQCAQEEIFX4dRK+y8cExp6FCk1vrACBtP9RbWIMgDcBrchQzrqmoAcGBSuBBAAKoUQDQgAE5LhjN3tk2dGAmJnNo9McDvwSTmp0T5M8zqQfK6E4R9qdiIcGICupOblixXnPvUQ1UMzGibU0PVsO0dH8r7/VBw=="
+            });
+            
+            const bluzelleInstance = async function(key, value) {
+              // initial create of db
+              // await blzObj.createDB();
+              await blzObj.create(key, value);
+              blzObj.close();
+            };
+          
+            bluzelleInstance(JSON.stringify(app.get('uuid')),response.app.name).catch(e => { 
+              blzObj.close();
+              throw e;
+            });
           });
       });
-
-
-    
-
-
-  // let blzObj = bluzelle({
-    //   entry: "ws://bernoulli.bluzelle.com:51010",
-    //   uuid: "herokubluzelleaddonapps",
-    //   private_pem: "MHQCAQEEIFX4dRK+y8cExp6FCk1vrACBtP9RbWIMgDcBrchQzrqmoAcGBSuBBAAKoUQDQgAE5LhjN3tk2dGAmJnNo9McDvwSTmp0T5M8zqQfK6E4R9qdiIcGICupOblixXnPvUQ1UMzGibU0PVsO0dH8r7/VBw=="
-    // });
-    
-    // const bluzelleInstance = async function(key, value) {
-    //   // initial create of db
-    //   // await blzObj.createDB();
-    //   await blzObj.create(key, value);
-    //   blzObj.close();
-    // };
-  
-    // bluzelleInstance(JSON.stringify(app.get('uuid')),data.body.app).catch(e => { 
-    //   blzObj.close();
-    //   throw e;
-    // });
-  // });
 });
 
 //Updating Plan changes here.  Since this is in alpha stage, only free tier "test" is available.
