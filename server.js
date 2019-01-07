b044d040681399015f5bb744714de467ce5640bf
Matt Ilagan <matthew@bluzelle.com>
2019-01-06 17:53:38 -0800

    Add delete storage functionality

--- server.js
+++ server.js
@@ -169,6 +169,27 @@ app.post('/heroku/resources', function handleProvisioning(req, res) {
       });
 });
 
+//Delete endpoint for which an add-on service is deleted
+app.delete('/heroku/resources/:id', function handleDelete(req, res) {
+  let blzObj = bluzelle({
+    entry: "ws://bernoulli.bluzelle.com:51010",
+    uuid: "herokubluzelleaddonapps",
+    private_pem: "MHQCAQEEIFX4dRK+y8cExp6FCk1vrACBtP9RbWIMgDcBrchQzrqmoAcGBSuBBAAKoUQDQgAE5LhjN3tk2dGAmJnNo9McDvwSTmp0T5M8zqQfK6E4R9qdiIcGICupOblixXnPvUQ1UMzGibU0PVsO0dH8r7/VBw=="
+  });
+  
+  const bluzelleInstance = async function(key) {
+    // initial create of db
+    // await blzObj.createDB();
+    await blzObj.delete(key);
+    blzObj.close();
+  };
+
+  bluzelleInstance(JSON.stringify(req.uuid)).catch(e => { 
+    blzObj.close();
+    throw e;
+  });
+});
+
 //Updating Plan changes here.  Since this is in alpha stage, only free tier "test" is available.
 //return a service unavailable response
 app.put('/heroku/resources/:id', function handlePlanChanges(req, res) {
